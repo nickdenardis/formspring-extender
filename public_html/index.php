f9bbeb38f8e9fe0db16f16a23360ac919157f352
nickdenardis <nick.denardis@gmail.com>
2010-12-19 16:30:10 -0500

    Added some basic style (taken and need to recreate) but the interface is starting to come together.

--- public_html/index.php
+++ public_html/index.php
@@ -6,5 +6,22 @@
 	<link href="/assets/main.css" media="all" rel="stylesheet" type="text/css" />
 </head>
 <body>
+<div id="container">
+	<header id="top">
+		<div class="wrap">
+			<h1><a href="/">Formspring Extender</a></h1>
+		</div>
+	</header>
+	<div id="main">
+		<div class="wrap">
+		
+		</div>
+	</div>
+	<footer>
+		<div class="wrap">
+			<p>Maintained by <a href="http://nickdenardis.com/">Nick DeNardis</a></p>
+		</div>
+	</footer>
+</div>
 </body>
 </html>
