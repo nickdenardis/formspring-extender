a890597f0a4217a48ceff6d47f4691d340a85c74
nickdenardis <nick.denardis@gmail.com>
2010-12-19 20:02:19 -0500

    Added some basic box style. Now on to adding some dynamic stuff.

--- public_html/index.php
+++ public_html/index.php
@@ -26,13 +26,20 @@
    	</header>
 	<section id="content">
 		<div class="wrap"><div class="wrap-inner">
-		 Content
+			<div class="box">
+				<header>
+					<h1>Manage Multiple Formspring Accounts!</h1>
+				</header>
+				<div class="box-content">
+		 			<p>Use this to connect multiple people to one formspring account.</p>
+		 		</div>
+			</div>
 		</div></div>
 	</section>
 	<footer>
-		<div class="wrap">
+		<div class="wrap"><div class="wrap-inner">
 			<p>Maintained by <a href="http://nickdenardis.com/">Nick DeNardis</a></p>
-		</div>
+		</div></div>
 	</footer>
 </div>
 </body>
