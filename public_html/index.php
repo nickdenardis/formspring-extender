032270ff90986bcf9db1c8954ab2d570c494f74e
nickdenardis <nick.denardis@gmail.com>
2010-12-19 19:52:39 -0500

    Added the style for the menu. Still needs some work.

--- public_html/index.php
+++ public_html/index.php
@@ -8,15 +8,27 @@
 <body>
 <div id="container">
 	<header id="top">
-		<div class="wrap">
+		<div class="wrap"><div class="wrap-inner">
 			<h1><a href="/">Formspring Extender</a></h1>
-		</div>
-	</header>
-	<div id="main">
-		<div class="wrap">
 		
-		</div>
-	</div>
+		<div id="subbar">
+          <nav id="primary">
+            <ul>
+              <li class="home selected"><a href="/">Home</a></li>
+              <li class="trends"><a href="/accounts">Accounts</a></li>
+              <li class="about"><a href="/signup">Signup</a></li>
+              <li class="about"><a href="/signin">Signin</a></li>
+            </ul>
+          </nav>
+        </div>
+        
+        </div></div>
+   	</header>
+	<section id="content">
+		<div class="wrap"><div class="wrap-inner">
+		 Content
+		</div></div>
+	</section>
 	<footer>
 		<div class="wrap">
 			<p>Maintained by <a href="http://nickdenardis.com/">Nick DeNardis</a></p>
